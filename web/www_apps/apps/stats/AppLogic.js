498c6b97723970c43ab2d23a734aeaccb47326d7
Dominik Mikiewicz <dominik@cartomatic.pl>
2014-08-06 18:25:03 +0200

    [www apps] stats - add grid to map

--- web/www_apps/apps/stats/AppLogic.js
+++ web/www_apps/apps/stats/AppLogic.js
@@ -16,6 +16,8 @@ Ext.define('stats.AppLogic', {
     
     launchApp: function(){
         
+        this.createDataModels();
+        
         //create the app layout
         this.buildLayout();
 
@@ -23,6 +25,28 @@ Ext.define('stats.AppLogic', {
         
     },
 
+    createDataModels: function(){
+        this.referrerModel = Ext.define('hgis.ReferrerStats', {
+            extend: 'Ext.data.Model', 
+            fields: [
+                {name: 'Referrer', type: 'string', defaultValue: null, useNull: true},
+                {name: 'GBytes', mapping: function(data) {return (data.Bytes / 1073741824).toFixed(8);}, type: 'number'},
+                {name: 'Hits', type: 'int', defaultValue: 0, useNull: false}
+            ]   
+        });
+        
+        this.ipModel = Ext.define('hgis.IpStats', {
+            extend: 'Ext.data.Model', 
+            fields: [
+                {name: 'Ip', type: 'string', defaultValue: null, useNull: true},
+                {name: 'Country', type: 'string', defaultValue: null, useNull: true},
+                {name: 'City', type: 'string', defaultValue: null, useNull: true},
+                {name: 'GBytes', convert: function(v, r) {if(r.data.Bytes)return (r.data.Bytes / 1073741824).toFixed(8);}, type: 'number'},
+                {name: 'Hits', type: 'int', defaultValue: 0, useNull: false},
+                {name: 'feature', type: 'auto', defaultValue: null, useNull: true}
+            ]
+        });
+    },
 
     /**
      * Builds application layout
@@ -63,21 +87,78 @@ Ext.define('stats.AppLogic', {
         
         //map panel
         this.mapPanel = Ext.create('Ext.panel.Panel', {
-            title: 'By Location',
+            title: 'Map',
             glyph: 'xf041@FontAwesome',
             layout: 'fit',
             itemId: 'map',
+            region: 'center',
+            margin: '5 0 5 5',
+            border: true,
             html: '<div id="mapcnt" style="position: absolute; width: 100%; height: 100%;"></div>'
         });
+        //update map size on resize
+        this.mapPanel.on('resize', function(){if(this.map)this.map.updateSize();},this);
         
-        this.referrerModel = Ext.define('hgis.ReferrerStats', {
-            extend: 'Ext.data.Model', 
-            fields: [
-                {name: 'Referrer', type: 'string', defaultValue: null, useNull: true},
-                {name: 'Bytes', type: 'number', defaultValue: null, useNull: true, convert: function(value, rec) {return (value ? (value / 1073741824) : 0);}},
-                {name: 'Hits', type: 'int', defaultValue: 0, useNull: false}
-            ]   
-        })
+        //grid panel
+        this.mapGridPanel = Ext.create('Ext.grid.Panel', {
+            title: 'Data',
+            glyph: 'xf0ce@FontAwesome',
+            region: 'east',
+            width: '50%',
+            margin: '5 5 5 0',
+            split: true,
+            collapsible: true,
+            titleCollapse: true,
+            columns: [
+                {header: 'Ip', flex: 1, dataIndex: 'Ip'},
+                {header: 'Country', flex: 1, dataIndex: 'Country'},
+                {header: 'City', flex: 1, dataIndex: 'City'},
+                {header: 'GBytes', flex: 1, dataIndex: 'GBytes'},
+                {header: 'Hits', flex: 1, dataIndex: 'Hits'}
+            ],
+            store: Ext.create('Ext.data.Store', {
+                model: this.ipModel,
+                data: []
+            })
+        });
+        //wire up some grid listeners
+        this.mapGridPanel.on(
+            'itemmouseenter',
+            function(grid, record, item, index, e, eOpts){
+                var f = record.get('feature');
+                if(f){
+                    //if feature present, highlight it
+                    this.highlightFeature(f);
+                }
+            },
+            this
+        );
+        
+        this.mapGridPanel.on(
+            'rowdblclick',
+            function(grid, record, tr, rowIndex, e, eOpts){
+                var f = record.get('feature');
+                if(f){
+                    this.map.getView().fitExtent(
+                        ol.extent.buffer(f.getGeometry().getExtent(), 500),
+                        this.map.getSize()
+                    );
+                }
+            },
+            this
+        );
+        
+        this.mapTab = Ext.create('Ext.panel.Panel', {
+            title: 'By Location',
+            glyph: 'xf041@FontAwesome',
+            layout: 'border',
+            itemId: 'map',
+            items: [
+                this.mapPanel,
+                this.mapGridPanel
+            ]
+        });
+        
 
         //grid panel
         this.gridPanel = Ext.create('Ext.grid.Panel', {
@@ -85,8 +166,8 @@ Ext.define('stats.AppLogic', {
             glyph: 'xf0c1@FontAwesome',
             itemId: 'grid',
             columns: [
-                {header: 'Referrer', flex: 1, dataIndex: 'Referrer'},
-                {header: 'GBytes', flex: 1, dataIndex: 'Bytes'},
+                {header: 'Referrer', flex: 1, renderer: this.referrerRenderer},
+                {header: 'GBytes', flex: 1, dataIndex: 'GBytes'},
                 {header: 'Hits', flex: 1, dataIndex: 'Hits'}
             ],
             store: Ext.create('Ext.data.Store', {
@@ -114,7 +195,7 @@ Ext.define('stats.AppLogic', {
                         }
                     ],
                     items: [
-                        this.mapPanel,
+                        this.mapTab,
                         this.gridPanel
                     ],
                     listeners: {
@@ -124,8 +205,6 @@ Ext.define('stats.AppLogic', {
             ]
         });
         
-        //update map size on viewport resize
-        this.viewport.on('resize', function(){this.map.updateSize();},this);
         
         this.mode = 'map';
         this.range = 'total';
@@ -133,6 +212,16 @@ Ext.define('stats.AppLogic', {
         this.getData();
     },
 
+    referrerRenderer: function(value, metaData, record, colIndex, store, view){
+        var url = record.get('Referrer');
+        if(url.startsWith('http://localhost')|| url.startsWith('http://127.0.0.1') || url.startsWith('unknown')){
+            return url;
+        }
+        else {
+            return '<a href="' + url + '"target="_blank">' + url + '</a>';
+        }
+    },
+    
     onTabChange: function(tabpanel, newC,oldC, eOpts){
         this.mode = newC.getItemId();
         this.getData();
@@ -212,86 +301,27 @@ Ext.define('stats.AppLogic', {
     
     loadMapData: function(data){
         
+        
         //first check the vector layer
         if(!this.vLayer){
             this.vLayer = new ol.layer.Vector({
                 source: new ol.source.Vector({
                     projection: ol.proj.get('EPSG:3857')
                 })
-                
                 //Note: so far no dynamic styles - for each feature a separate style needs to be created
                 //so styles get assigned to a feature rather than layer at this stage
-//                style: function(feature, resolution) {
-//				    var text = resolution < 5000 ? feature.get('name') : '';
-//				    if (!styleCache[text]) {
-//				      styleCache[text] = [new ol.style.Style({
-//				        fill: new ol.style.Fill({
-//				          color: 'rgba(255, 255, 255, 0.6)'
-//				        }),
-//				        stroke: new ol.style.Stroke({
-//				          color: '#319FD3',
-//				          width: 1
-//				        }),
-//				        text: new ol.style.Text({
-//				          font: '12px Calibri,sans-serif',
-//				          text: text,
-//				          fill: new ol.style.Fill({
-//				            color: '#000'
-//				          }),
-//				          stroke: new ol.style.Stroke({
-//				            color: '#fff',
-//				            width: 3
-//				          })
-//				        })
-//				      })];
-//				    }
-//				    return styleCache[text];
-//				  }
-
-                
-                //style the circles depending on the incoming data
-//                style: (
-//                    function(){
-//                        
-//                        var getTxt = function(f){
-//                            
-//                        }
-//                        
-//                        var getSize = function(f){
-//                            
-//                        }
-//                        
-//                        return function(feature, resolution){
-//                            
-//                        }
-//                    }()
-//                )
-//                
-//                
-//                /*new ol.style.Style({
-//                    image: new ol.style.Circle({
-//			          radius: 10,
-//			          stroke: new ol.style.Stroke({
-//			            color: '#fff'
-//			          }),
-//			          fill: new ol.style.Fill({
-//			            color: '#3399CC'
-//			          })
-//			        }),
-//                    text: new ol.style.Text({
-//                        text: 'dupa',
-//                        stroke: new ol.style.Stroke({
-//                        color: '#fff'
-//                      }),
-//                      fill: new ol.style.Fill({
-//                        color: '#3399CC'
-//                      })
-//                    })
-//                })*/
-//               
             });
             
             this.map.addLayer(this.vLayer);
+            
+            
+            //wire up mouse move to trigger highlightFeature
+            Ext.get(this.map.getViewport()).on(
+                'mousemove',
+                this.findAndHighlightFeature,
+                this
+            );
+            
         }
         
         var src = this.vLayer.getSource(),
@@ -301,12 +331,11 @@ Ext.define('stats.AppLogic', {
         //wipe out the layer
         src.clear();
         
-        //
-        
         
         var d = 0,
             dlen = data.length,
             features = [],
+            recs = [],
             maxSize = dlen > 0 ? data[0].Bytes : 0
             maxCircleSize = 70; //radius of 70px
         
@@ -329,7 +358,10 @@ Ext.define('stats.AppLogic', {
         }
         
         for(d; d < dlen; d++){
-            if(!data[d].Lon) continue; //ignore recs withou geo
+            if(!data[d].Lon) continue; //ignore recs without geo
+            
+            var r = Ext.create(this.ipModel, data[d]);
+            recs.push(r);
             
             var f = new ol.Feature({
                 geometry: new ol.geom.Point([data[d].Lon, data[d].Lat]).transform(tsrc, tdest)
@@ -337,29 +369,36 @@ Ext.define('stats.AppLogic', {
                 //not exported api property or something, so setting the style through setStyle
                 //on a feature object
             });
+            f.setProperties({
+                rec: r
+            });
             f.setStyle([
                 new ol.style.Style({
-					image: new ol.style.Circle({
-						radius: getRadius(data[d]),
-						stroke: new ol.style.Stroke({
-	                        color: '#fff'
-						}),
-						fill: new ol.style.Fill({
-						    color: 'rgba(51,152,153,0.5)'
-					    })
-					}),
-					text: new ol.style.Text({
-					   text: getTopText(data[d]),
+                    image: new ol.style.Circle({
+                        radius: getRadius(data[d]),
+                        stroke: new ol.style.Stroke({
+                            color: '#fff'
+                        }),
+                        fill: new ol.style.Fill({
+                            color: 'rgba(51,152,153,0.5)'
+                        })
+                    })
+                })
+                //txt styles - not use at the time being
+                /*,
+                new ol.style.Style({
+                    text: new ol.style.Text({
+                       text: getTopText(data[d]),
                        font: 'bold 14px Arial',
                        offsetY: -7,
-					   stroke: new ol.style.Stroke({
-					       color: '#fff'
-					   }),
-					   fill: new ol.style.Fill({
-					       color: 'rgb(153,51,102)'
-					   })
-					})
-				}),
+                       stroke: new ol.style.Stroke({
+                           color: '#fff'
+                       }),
+                       fill: new ol.style.Fill({
+                           color: 'rgb(153,51,102)'
+                       })
+                    })
+                }),
                 new ol.style.Style({
                     text: new ol.style.Text({
                        text: getBottomText(data[d]),
@@ -374,9 +413,10 @@ Ext.define('stats.AppLogic', {
                            color: '#000'
                        })
                     })
-                })
+                })*/
             ]);
             features.push(f);
+            r.set('feature', f);
         }
         src.addFeatures(features);
         
@@ -386,6 +426,65 @@ Ext.define('stats.AppLogic', {
             this.map.getSize()
         );
         
+        //also load grid
+        this.mapGridPanel.getStore().loadRecords(recs);
+    },
+    
+    
+    
+    
+    findAndHighlightFeature: function(e){
+        
+        var px = this.map.getEventPixel(e.event);
+        
+        var feature = this.map.forEachFeatureAtPixel(px, function(feature, layer) {
+            return feature;
+        });
+        
+        this.highlightFeature(feature);
+    },
+    
+    highlight: null,
+    
+    highlightFeature: function(f){
+        
+        this.ensureFeatureOverlay();
+        
+        
+        if(f!=this.highlight){
+            if(this.highlight){
+                this.featureOverlay.removeFeature(this.highlight);
+            }
+            if (f) {
+                this.featureOverlay.addFeature(f);
+                
+                this.mapGridPanel.getSelectionModel().select(f.getProperties().rec);
+            }
+            this.highlight = f;
+        }
+    },
+    
+    ensureFeatureOverlay: function(){
+        if(!this.featureOverlay){
+            this.featureOverlay = new ol.FeatureOverlay({
+                map: this.map,
+                style: function(f,r){
+                    return [
+                        new ol.style.Style({
+                            image: new ol.style.Circle({
+                                radius: f.getStyle()[0].getImage().getRadius(), //read the radius off the feature style
+                                stroke: new ol.style.Stroke({
+                                    color: '#fff'
+                                }),
+                                fill: new ol.style.Fill({
+                                    color: 'rgba(130,90,153,0.5)'
+                                })
+                            })
+                        })
+                    ];
+                }
+            });
+        }
     },
     
     /**
